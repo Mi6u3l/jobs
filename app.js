97670aa1751a4a7d98290cd9fd2a4c2f305d3c59
Julius Suominen <julius.suominen@gmail.com>
2017-08-02 13:06:47 +0200

    dist

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/dist')));
 
 app.use('/auth', auth);
 app.use('/api', api);
